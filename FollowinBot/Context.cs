a0381b0703c7cc9f384a9409de3cd12af4db6a65
Eridani 8 <tsuuzetsu@gmail.com>
2025-03-31 10:49:30 +0300

    added env and db

---
+++ FollowinBot/Context.cs
@@ -0,0 +1,20 @@
+﻿using LiteDB;
+
+namespace FollowinBot;
+
+public class Context
+{
+    public ILiteCollection<NewsEntity> News { get; }
+    
+    public Context()
+    {
+        var db = new LiteDatabase("data.db");
+        News = db.GetCollection<NewsEntity>("news");
+    }
+}
+
+public class NewsEntity
+{
+    public required string Title { get; set; }
+    public required string Text { get; set; }
+}
--- FollowinBot/Program.cs
+++ FollowinBot/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
+using DotNetEnv;
+using FollowinBot;
+using Microsoft.Extensions.DependencyInjection;
+using Telegram.Bot;
 
 const string outputTemplate = "[{Timestamp:yyyy-MM-dd HH:mm:ss}] [{Level:u3}] {Message:lj}{NewLine}{Exception}";
 var logsPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "logs"));
@@ -21,9 +25,24 @@ Log.Logger = new LoggerConfiguration()
 
 try
 {
+    #region env
+
+    Env.Load();
+    
+    var botToken = Environment.GetEnvironmentVariable("BOT_TOKEN");
+
+    if (string.IsNullOrEmpty(botToken))
+    {
+        throw new Exception("Environment variable 'BOT_TOKEN' not set");
+    }
+    
+    #endregion
+    
     var builder = Host.CreateApplicationBuilder();
     
     builder.Services.AddSerilog();
+    builder.Services.AddSingleton<ITelegramBotClient, TelegramBotClient>(_ => new TelegramBotClient(botToken));
+    builder.Services.AddSingleton<Context>();
     
     var host = builder.Build();
 
